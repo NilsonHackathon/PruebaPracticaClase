c6441411acb6972f66f34ce1509aa0e6aae788c8
Nilson José <117861033+NilsonHackathon@users.noreply.github.com>
2024-11-03 19:47:56 -0500

    Primer commit y diseño de pantallas

--- PruebaPracticaClase/ViewController.swift
+++ PruebaPracticaClase/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func RegisterButtonAction(_ sender: Any) {
+    }
 }
 
